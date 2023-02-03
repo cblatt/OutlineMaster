d640183d494a664f69193e76e6f290b1b9873dd2
Hannah Lyytikainen <hlyytika@uwo.ca>
2023-02-02 20:01:20 -0500

    initial navbars

---
+++ packages/frontend/src/components/Navigation/AdminNav.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import { Nav, Navbar, NavLink } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+
+export default function AdminNav() {
+  
+
+  return (
+    <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
+      <Navbar.Toggle
+        aria-controls="navbarScroll"
+        data-bs-target="#navbarScroll"
+      />
+      <Navbar.Collapse>
+        <Nav>
+          
+            <NavLink eventKey="1" as={Link} to="/homepage">
+              Homepage
+            </NavLink>
+          
+          
+            <NavLink eventKey="2" as={Link} to="/prev-outlines">
+              Previous Outlines
+            </NavLink>
+          
+          
+            <NavLink eventKey="3" as={Link} to="/outline-history">
+              Outline History
+            </NavLink>
+          
+          
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
