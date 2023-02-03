5f230ef986d795e61bc31db5d3fe34de80625c2b
rwill33 <90706510+rwill33@users.noreply.github.com>
2023-02-03 15:15:08 -0500

    Hashes passwords in database using bcrypt

--- packages/backend/README.md
+++ packages/backend/README.md
@@ -71,3 +71,7 @@ Nest is an MIT-licensed open source project. It can grow thanks to the sponsors 
 ## License
 
 Nest is [MIT licensed](LICENSE).
+
+## Tutorials
+
+https://www.prisma.io/blog/nestjs-prisma-rest-api-7D056s1BmOL0
--- packages/backend/src/users/users.service.ts
+++ packages/backend/src/users/users.service.ts
@@ -1,14 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
-  create(createUserDto: CreateUserDto) {
-    return this.prisma.user.create({ data: createUserDto });
+  hashPassword(data: string) {
+    return bcrypt.hash(data, 10);
+  }
+
+  async create(createUserDto: CreateUserDto) {
+    const hashedPassword = await this.hashPassword(createUserDto.password);
+
+    return this.prisma.user.create({
+      data: {
+        ...createUserDto,
+        password: hashedPassword,
+      },
+    });
   }
 
   findAll() {
@@ -17,6 +29,20 @@ export class UsersService {
 
   findOne(id: string) {
     return this.prisma.user.findUnique({ where: { uwoId: id } });
+  }
+
+  async login(id: string, password: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { uwoId: id },
+    });
+
+    if (!user) throw new ForbiddenException('Access Denied');
+
+    const passwordMatches = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatches) throw new ForbiddenException('Access Denied');
+
+    return user;
   }
 
   update(id: string, updateUserDto: UpdateUserDto) {
