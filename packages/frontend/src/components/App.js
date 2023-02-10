fd355a95e541457a8ee89c333f80392de3184efd
Brayden Thompson <bthomp89@uwo.ca>
2023-02-09 20:45:06 -0500

    added page for adding instructor

--- packages/frontend/src/components/App.js
+++ packages/frontend/src/components/App.js
@@ -4,6 +4,8 @@ import InstructorHome from "./InstructorHome";
 import Login from "./Login";
 import AdminHome from "./AdminHome";
 import { AdminGuard } from "../guards/AdminGuard";
+import UnAuth from "./UnAuth";
+import AddInstructor from "./AddInstructor";
 
 function App() {
   return (
@@ -22,6 +24,14 @@ function App() {
                 exact
                 path="/admin-home"
                 element={<AdminHome></AdminHome>}
+              />
+            </Route>
+            <Route exact path="/unauth" element={<UnAuth></UnAuth>} />
+            <Route element={<AdminGuard />}>
+              <Route
+                exact
+                path="/admin-add"
+                element={<AddInstructor></AddInstructor>}
               />
             </Route>
           </Routes>
---
+++ packages/frontend/src/components/UnAuth.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function UnAuth() {
+  return (
+    <div>
+      <h4>UNAUTH</h4>
+    </div>
+  );
+}
