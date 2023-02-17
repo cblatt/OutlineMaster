863b66a34a001f295446c1087396f565fe8f31f7
cblatt <cblatt@uwo.ca>
2023-02-17 15:06:48 -0500

    Create/Edit outline - Unfinished

--- packages/frontend/src/components/App.js
+++ packages/frontend/src/components/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import InstructorHome from "./InstructorHome";
 import Login from "./Login";
 import AdminHome from "./AdminHome";
+import CreateOutline from "./CreateOutline";
+import OutlineEditor from "./OutlineEditor";
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
               element={<InstructorHome></InstructorHome>}
             />
             <Route exact path="/admin-home" element={<AdminHome></AdminHome>} />
+            <Route exact path="/create-outline" element={<CreateOutline></CreateOutline>} />
+            <Route exact path="/editor" element={<OutlineEditor></OutlineEditor>} />
           </Routes>
         </Router>
       </div>
