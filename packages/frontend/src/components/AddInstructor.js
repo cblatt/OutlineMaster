5168e8431f5b33e3a3ba309661a902d1c85bc333
rwill33 <90706510+rwill33@users.noreply.github.com>
2023-02-17 00:42:47 -0500

    Adds form submission to add instructor form

--- packages/frontend/src/components/AddInstructor.js
+++ packages/frontend/src/components/AddInstructor.js
@@ -8,10 +8,39 @@ import {
   Button,
 } from "@chakra-ui/react";
 import AdminNav from "./AdminNav";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
 export default function AddInstructor() {
   const [show, setShow] = useState(false);
   const handleClick = () => setShow(!show);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const navigate = useNavigate();
+
+  async function onSubmit(data) {
+    fetch("/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Content-length": 7,
+      },
+      body: JSON.stringify({ role: "INSTRUCTOR", ...data }),
+    })
+      .then((res) => {
+        if (res.status === 201) {
+          return res.json();
+        }
+      })
+      .then((user) => {
+        if (user) {
+          navigate("/admin-home", { replace: true });
+        }
+      });
+  }
 
   return (
     <div className="min-h-screen bg-gradient-to-r from-purple-500 to-purple-300 ... ">
@@ -21,13 +50,14 @@ export default function AddInstructor() {
           <h4 className="text-4xl text-gray-700 font-semibold text-center py-5">
             Add an Instructor
           </h4>
-          <FormControl isRequired>
+          <FormControl isRequired={errors}>
             <Stack spacing={5}>
               <Input
                 type="text"
                 size="lg"
                 variant="outline"
                 placeholder="UWO ID"
+                {...register("uwoId", { required: true })}
               />
 
               <Input
@@ -35,6 +65,7 @@ export default function AddInstructor() {
                 size="lg"
                 variant="outline"
                 placeholder="Email"
+                {...register("email", { required: true })}
               />
               <InputGroup>
                 <Input
@@ -42,6 +73,7 @@ export default function AddInstructor() {
                   size="lg"
                   type={show ? "text" : "password"}
                   placeholder="Enter password"
+                  {...register("password", { required: true })}
                 />
                 <InputRightElement width="4.5rem">
                   <Button
@@ -59,14 +91,19 @@ export default function AddInstructor() {
                 size="lg"
                 variant="outline"
                 placeholder="First Name"
+                {...register("firstName", { required: true })}
               />
               <Input
                 type="text"
                 size="lg"
                 variant="outline"
                 placeholder="Last Name"
+                {...register("lastName", { required: true })}
               />
-              <Button className="text-gray-700 rounded-md hover:opacity-100">
+              <Button
+                className="text-gray-700 rounded-md hover:opacity-100"
+                onClick={handleSubmit(onSubmit)}
+              >
                 Add Instructor
               </Button>
             </Stack>
