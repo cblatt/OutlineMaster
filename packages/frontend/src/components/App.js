f53313ef5e4ae25f8aadc4a184af4c468f641222
rwill33 <90706510+rwill33@users.noreply.github.com>
2023-02-18 16:19:18 -0500

    Changes routing, adds instructor and user guard, and adds buttons to unauth page

--- packages/frontend/src/components/App.js
+++ packages/frontend/src/components/App.js
@@ -3,10 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import InstructorHome from "./InstructorHome";
 import Login from "./Login";
 import AdminHome from "./AdminHome";
-import { AdminGuard } from "../guards/AdminGuard";
+import { AdminGuard, InstructorGuard, UserGuard } from "../guards/Guard";
 import UnAuth from "./UnAuth";
 import AssignInstructor from "./AssignInstructor";
-
 import AddInstructor from "./AddInstructor";
 import Logout from "./Logout";
 
@@ -17,33 +16,34 @@ function App() {
         <Router>
           <Routes>
             <Route exact path="/" element={<Login></Login>} />
-            <Route
-              exact
-              path="/home"
-              element={<InstructorHome></InstructorHome>}
-            />
-            <Route element={<AdminGuard />}>
-              <Route
-                exact
-                path="/admin-home"
-                element={<AdminHome></AdminHome>}
-              />
-            </Route>
-            <Route exact path="/unauth" element={<UnAuth></UnAuth>} />
-            <Route exact path="/logout" element={<Logout></Logout>}></Route>
-            <Route element={<AdminGuard />}>
-              <Route
-                exact
-                path="/admin-add"
-                element={<AddInstructor></AddInstructor>}
-              />
-            </Route>
-            <Route element={<AdminGuard />}>
-              <Route
-                exact
-                path="/assign"
-                element={<AssignInstructor></AssignInstructor>}
-              />
+            <Route element={<UserGuard />}>
+              <Route element={<InstructorGuard />}>
+                <Route
+                  exact
+                  path="/home"
+                  element={<InstructorHome></InstructorHome>}
+                />
+              </Route>
+
+              <Route element={<AdminGuard />}>
+                <Route
+                  exact
+                  path="/admin-home"
+                  element={<AdminHome></AdminHome>}
+                />
+                <Route
+                  exact
+                  path="/admin-add"
+                  element={<AddInstructor></AddInstructor>}
+                />
+                <Route
+                  exact
+                  path="/assign"
+                  element={<AssignInstructor></AssignInstructor>}
+                />
+              </Route>
+              <Route exact path="/unauth" element={<UnAuth></UnAuth>} />
+              <Route exact path="/logout" element={<Logout></Logout>}></Route>
             </Route>
           </Routes>
         </Router>
