395f1affd82c0ec26ddfb9618d29e7f0bae0809e
Brayden Thompson <bthomp89@uwo.ca>
2023-03-23 20:10:04 -0400

    updated admin nav

--- packages/frontend/src/components/AdminNav.js
+++ packages/frontend/src/components/AdminNav.js
@@ -27,17 +27,9 @@ export default function AdminNav() {
           <NavLink eventKey="1" as={Link} to="/admin-home">
             Homepage
           </NavLink>
-          <NavLink eventKey="3" as={Link} to="/outline-history">
-            Outline History
-          </NavLink>
           <NavLink eventKey="4" as={Link} to="/admin-add">
             Add Instructor
           </NavLink>
-
-          <NavLink eventKey="5" as={Link} to="/assign">
-            Assign Instructor
-          </NavLink>
-
           <NavLink eventKey="6" as={Link} to="/courses">
             Courses
           </NavLink>
