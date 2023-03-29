ce57e049753bd9151730e98ede8e25b63f81c6ec
rwill33 <90706510+rwill33@users.noreply.github.com>
2023-03-28 23:09:59 -0400

    Backend clean up

--- packages/backend/src/editor-log/editor-log.service.ts
+++ packages/backend/src/editor-log/editor-log.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateEditorLogDto } from './dto/create-editor-log.dto';
-import { UpdateEditorLogDto } from './dto/update-editor-log.dto';
 
 @Injectable()
 export class EditorLogService {
@@ -11,21 +10,5 @@ export class EditorLogService {
     return this.prisma.editLog.create({
       data: createEditorLogDto,
     });
-  }
-
-  findAll() {
-    return this.prisma.editLog.findMany();
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} editorLog`;
-  }
-
-  update(id: number, updateEditorLogDto: UpdateEditorLogDto) {
-    return `This action updates a #${id} editorLog`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} editorLog`;
   }
 }
